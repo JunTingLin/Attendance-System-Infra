976e10ea8c22e457e0bdfdb3cb16a8d93d80bc75
junting <junting0218@gmail.com>
2025-05-18 22:37:19 +0800

    feat: copy existing state to the new backend(GCS)

--- provider.tf
+++ provider.tf
@@ -2,3 +2,11 @@ provider "google" {
   project = "tsmc-attendance-system-458811"
   region  = "asia-east1"
 }
+
+terraform {
+  # Configure Terraform to store state in GCS
+  backend "gcs" {
+    bucket = "attendance-system-terraform-state"    # <— replace with your bucket name
+    prefix = "infra/state"                          # <— path inside bucket for tfstate file
+  }
+}
