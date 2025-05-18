82d0dce645a22b39c9d68ff64906f32edb0356d9
jhen-fang <jen.futurenest@gmail.com>
2025-05-18 21:28:41 +0800

    update frontend repo name

--- cloudbuild_trigger.tf
+++ cloudbuild_trigger.tf
@@ -1,12 +1,13 @@
 resource "google_cloudbuild_trigger" "frontend_build_trigger" {
   name        = "frontend-build-trigger"
-  description = "Trigger for building and deploying Attendance Frontend from GitHub"
+  description = "Trigger for building and deploying Attendance Frontend (1st Gen)"
+  location    = "global"
 
   service_account = "projects/tsmc-attendance-system-458811/serviceAccounts/terraform-junting@tsmc-attendance-system-458811.iam.gserviceaccount.com"
 
   github {
     owner = "jhen-fang"
-    name  = "Attendance-System-Frontend"
+    name  = "Attendance-System-frontend"
     push {
       branch = "^main$"
     }
